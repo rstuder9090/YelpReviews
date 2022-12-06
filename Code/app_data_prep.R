4f120471a43f0eef0112ac99d3e9fe115c2ac803
Jack Bressett <38769118+BressettJ21@users.noreply.github.com>
2022-12-06 10:02:52 -0600

    Updated to generate LDA analysis by business
    Files will be loaded into app rather than computing on the spot

--- Code/app_data_prep.R
+++ Code/app_data_prep.R
@@ -45,4 +45,152 @@ write.csv(stars_all_reviews, 'app_data/stars_all_reviews.csv')
 ################################################################################################################
 
 
+#Put LDA findings altogether in an organized format: create individual analysis for each business then save results into file system.
+#Reviews first
+
+library(tidyverse)
+library(tidytext)
+library(topicmodels)
+
+reviews <- read_csv("sushi_reviews.csv"); colnames(reviews)[1] <- "index"
+tips <- read_csv("sushi_tips.csv"); colnames(tips)[1] <- "index"
+
+business <- read_csv("sushi.csv")
+
+
+
+extra_stop_words <- c('sushi', 'food', 'rolls', 'place', 'just', 'one')  #from n-gram analysis
+test <- cbind(extra_stop_words,rep("CUSTOM", 6))
+test <- data.frame(test); colnames(test) <- c('word','lexicon')
+stop_words <- rbind(stop_words,test)
+
+standout_reviews <- data.frame(matrix(ncol=4)); colnames(standout_reviews) <- c("business_id","text", "topi", "type")
+
+for(i in business$business_id){
+  
+  for(k in c('good', 'bad')){
+    print(k)
+    if(k == 'good'){ 
+      temp <- reviews %>% 
+        filter(business_id==i & stars > 3)
+    } 
+    else{
+      temp <- reviews %>% 
+        filter(business_id==i & stars < 3)
+    }
+    if(nrow(temp) > 10){
+      word_counts <- temp %>%
+        unnest_tokens(word,V10) %>%
+        anti_join(stop_words) %>% 
+        count(index,word)
+      
+      reviews_dtm <- word_counts %>%
+        cast_dtm(index, word, n)
+      
+      reviews_lda <- LDA(reviews_dtm, k = 2, control = list(seed = 42))
+      
+      
+      topics <- tidy(reviews_lda, matrix = "beta")
+      
+      memberships <- tidy(reviews_lda, matrix = "gamma")
+      prepath <- 'app_data/review-LDA/'
+      path <- paste0(i,k, sep='-')
+      path <- paste0(path, '.rda')
+      path <- paste0(prepath,path)
+      
+      save(topics, memberships, file = path)
+      
+      for(t in 1:2){
+        location <- memberships %>%
+          arrange(document, topic) %>% 
+          filter(topic==t) %>% 
+          slice_max(gamma, n = 1) %>% 
+          select(document)
+        
+        location <- min(as.numeric(location[[1]]))
+        
+        temp2 <- temp %>% 
+          filter(index == location) %>% 
+          select(business_id, text) %>% 
+          mutate(topi = t, type = k)
+        standout_reviews <- rbind(standout_reviews, temp2)
+        
+      }#close topics loop
+      
+    }#close length condition
+  }#closes good and bad loop
+  
+}#closes business_id loop
+
+
+quote_path <- paste0('app_data/quotes/','quote_reviews')
+quote_path <- paste0(quote_path,'.csv')
+print(quote_path)
+write.csv(standout_reviews, quote_path)
+
+
+#Same structure applied to tips data 
+
+extra_stop_words <- c('sushi', 'food', 'rolls', 'place', 'just', 'one')  #from n-gram analysis
+test <- cbind(extra_stop_words,rep("CUSTOM", 6))
+test <- data.frame(test); colnames(test) <- c('word','lexicon')
+stop_words <- rbind(stop_words,test)
+
+standout_tips <- data.frame(matrix(ncol=3)); colnames(standout_tips) <- c("business_id","text", "topi")
+
+for(i in business$business_id){
+  
+  temp <- tips %>% 
+    filter(business_id==i)
+  
+  if(nrow(temp) > 5){
+    word_counts <- temp %>%
+      unnest_tokens(word,V6) %>%
+      anti_join(stop_words) %>% 
+      count(index,word)
+    
+    tips_dtm <- word_counts %>%
+      cast_dtm(index, word, n)
+    
+    tips_lda <- LDA(tips_dtm, k = 2, control = list(seed = 42))
+    
+    
+    topics <- tidy(tips_lda, matrix = "beta")
+    
+    memberships <- tidy(tips_lda, matrix = "gamma")
+    prepath <- 'app_data/tips-LDA/'
+    path <- paste0(i, '.rda')
+    path <- paste0(prepath,path)
+    
+    save(topics, memberships, file = path)
+    
+    for(t in 1:2){
+      location <- memberships %>%
+        arrange(document, topic) %>% 
+        filter(topic==t) %>% 
+        slice_max(gamma, n = 1) %>% 
+        select(document)
+      
+      location <- min(as.numeric(location[[1]]))
+      
+      temp2 <- temp %>% 
+        filter(index == location) %>% 
+        select(business_id, text) %>% 
+        mutate(topi = t)
+      standout_tips <- rbind(standout_tips, temp2)
+      
+    }#close topics loop
+    
+  }#close length condition
+  
+}#closes business_id loop
+
+
+quote_path <- paste0('app_data/quotes/','quote_tips')
+quote_path <- paste0(quote_path,'.csv')
+print(quote_path)
+write.csv(standout_tips, quote_path)
+
+
+
 
