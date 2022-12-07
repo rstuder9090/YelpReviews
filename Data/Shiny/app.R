2e71c93bac5a1882d3cea169101eab53218c8102
Jack Bressett <38769118+BressettJ21@users.noreply.github.com>
2022-12-07 15:55:54 -0600

    Added Topics Playground server+ui, Added data

--- Data/Shiny/app.R
+++ Data/Shiny/app.R
@@ -4,10 +4,15 @@ library(ggplot2)
 library(shinyWidgets)
 library(shinydashboard)
 
-
+#Descriptive data  
 sushi<- read_csv("app_business_data.csv"); sushi <- rename(sushi, index = `...1`)
-
 sushi_reviews_stars <- read_csv("stars_all_reviews.csv"); sushi_reviews_stars <- rename(sushi_reviews_stars, index = `...1`)
+#Topics Playground Data
+good_review_grams <- read_csv('good_reviews_grams.csv'); good_review_grams <- rename(good_review_grams, index = `...1`)
+bad_review_grams <- read_csv('bad_reviews_grams.csv'); bad_review_grams <- rename(bad_review_grams, index = `...1`)
+tips_grams <- read_csv('tips_grams.csv'); tips_grams <- rename(tips_grams, index = `...1`)
+quote_reviews <- read_csv('LDA/quotes/quote_reviews.csv')
+tip_reviews <- read_csv('LDA/quotes/quote_tips.csv')
 
 ui<- fluidPage(
   useShinydashboard(),
@@ -29,13 +34,36 @@ ui<- fluidPage(
                           # more basics about restaurant performance - compared to average sushi?
                           )),
                       ), #end tab 1
-              tabPanel("Topic Analysis", fluid=TRUE,
-                      sidebarPanel(titlePanel("Title"),
-                                   selectInput("", "", choices = NULL)
+               tabPanel("Topic Playground", fluid=TRUE,
+                      sidebarPanel(titlePanel(htmlOutput("name") ),
+                                   selectInput("data", "Choose which insights to view", choices = c("Reviews","Tips"), selected = "Reviews"), 
+                                   strong('Below are quotes from users that most embody the topics of your business:'),
+                                   br(),
+                                   p('Warning: Chosen restaurant must have over 10 good or 10 bad reviews and 20 tips otherwise table will not appear'),
+                                   br(),
+                                   htmlOutput("quote_header"),
+                                   htmlOutput("review_quotes")
                       ),
-                      mainPanel(
-                        #Topics data
-                               )
+                      mainPanel(conditionalPanel(condition = "input.data == 'Reviews'", 
+                                                 verticalLayout(
+                                                   strong("Popular Topics from N-grams --- Left: Good Reviews with stars > 3. ------------------ ------------------  Right: Bad Review with stars < 3"),
+                                                   splitLayout(cellWidths = c("50%", "50%"), dataTableOutput("good_reviews_table"), dataTableOutput("bad_reviews_table")), br(),
+                                                   strong("Unique Topics from LDA ---Left: Unique Topics in Good Reviews. ------------------------------------ Right: Unique Topics in Bad Reviews "),
+                                                   splitLayout(cellWidths = c("50%", "50%"), dataTableOutput("good_review_topics_table"), dataTableOutput("bad_review_topics_table"))
+                                                 ) #close vert layout - reviews
+                                                 
+                                                 ), #close conditional panel - reviews
+          
+                                conditionalPanel(condition = "input.data == 'Tips'", 
+                                verticalLayout(
+                                  strong("Popular Topics from N-grams on Tips Dataset"),
+                                  dataTableOutput("tips_table"), br(),
+                                  strong("Unique Topics from LDA on Tips Dataset"),
+                                  dataTableOutput("tips_topics_table")
+                                ) #close vert layout - tips
+                                
+                      ) #close conditional panel - tips
+                      )#close main panel
                       ), # end tab 2
              
              tabPanel("Likes & Dislikes", fluid=TRUE,
@@ -185,7 +213,174 @@ server<- function(input, output, session) {
        main = "Emotions in Reviews", xlab="Percentage") 
    }
  }) 
- 
+   ##############End stats page logic
+  #helper functions for LDA discriminative  topics
+  kl_div <- function(p1, p2) {
+    p1 * log(p1 / p2) + (p2 - p2)
+  }
+  kl_mat <- function(p) {
+    K <- matrix(0, nrow = length(p), ncol = length(p))
+    
+    for (i in seq_along(p)) {
+      for (j in seq_len(i - 1)) {
+        K[i, j] <- kl_div(p[i], p[j])
+      }
+    }
+    K
+  }
+  discrepancy <- function(p, lambda = 1e-7) {
+    p <- (p + lambda) / sum(p + lambda) # Laplace smoothing
+    K <- kl_mat(p)
+    max(K)
+  }
+  
+  
+  output$name <- renderText({
+    aa <- paste0("Explore Popular and Unique Topics for ","<font color=\"#FF0000\"><b>", input$name, " in ", input$city, ", ", input$state, ".") 
+    paste0(aa,"<font color=\"000000\"><b>","<br> Please Select: Yelp Reviews  or Yelp Tips.")
+    })
+  
+  
+  business_ID <- reactive({
+      id <- sushi %>% 
+        filter(name == input$name) %>% 
+        select(business_id)
+      return(id[[1]])
+  })
+
+#Good Review N-gram    
+output$good_reviews_table <- renderDataTable({
+  id <- business_ID()
+  current_business_good <- good_review_grams %>% 
+    filter(business_id==id) %>% 
+    select(-c(index, business_id))
+  
+}, options = list(pageLength=10))
+
+#Bad Review N-gram    
+output$bad_reviews_table <- renderDataTable({
+  id <- business_ID()
+  current_business_bad <- bad_review_grams %>% 
+    filter(business_id==id) %>% 
+    select(-c(index, business_id))
+  
+},options = list(pageLength=10))  
+
+#Tip N-gram
+output$tips_table <- renderDataTable({
+  id <- business_ID()
+  current_business_tips <- tips_grams %>% 
+    filter(business_id==id) %>% 
+    select(-c(index, business_id))
+  
+}, options = list(pageLength=10))
+
+#Good Review LDA 
+output$good_review_topics_table <- renderDataTable({
+  id <- business_ID()
+  prepath <- 'LDA/review-LDA/'
+  path <- paste0(prepath,id,"good-.rda")
+  loaded_good <- load(path)
+  
+  discriminative_terms <- topics %>%
+    group_by(term) %>%
+    mutate(D = discrepancy(beta)) %>%
+    ungroup() %>%
+    slice_max(D, n = 25) %>%
+    mutate(term = fct_reorder(term, -D))
+  
+  
+  discriminative_terms <- discriminative_terms %>%
+    select(-D) %>%
+    pivot_wider(names_from = "topic", values_from = "beta") 
+  
+  discriminative_terms %>% 
+    mutate(rank = 1:nrow(discriminative_terms)) %>% 
+    select(term,rank)
+  
+})
+
+#Bad Review LDA 
+output$bad_review_topics_table <- renderDataTable({
+  id <- business_ID()
+  prepath <- 'LDA/review-LDA/'
+  path <- paste0(prepath,id,"bad-.rda")
+  loaded_good <- load(path)
+  
+  discriminative_terms <- topics %>%
+    group_by(term) %>%
+    mutate(D = discrepancy(beta)) %>%
+    ungroup() %>%
+    slice_max(D, n = 25) %>%
+    mutate(term = fct_reorder(term, -D))
+  
+  
+  discriminative_terms <- discriminative_terms %>%
+    select(-D) %>%
+    pivot_wider(names_from = "topic", values_from = "beta") 
+  
+  discriminative_terms %>% 
+    mutate(rank = 1:nrow(discriminative_terms)) %>% 
+    select(term,rank)
+})  
+
+#Tips LDA 
+output$tips_topics_table <- renderDataTable({
+  id <- business_ID()
+  prepath <- 'LDA/tips-LDA/'
+  path <- paste0(prepath,id,".rda")
+  loaded_good <- load(path)
+  
+  discriminative_terms <- topics %>%
+    group_by(term) %>%
+    mutate(D = discrepancy(beta)) %>%
+    ungroup() %>%
+    slice_max(D, n = 25) %>%
+    mutate(term = fct_reorder(term, -D))
+  
+  
+  discriminative_terms <- discriminative_terms %>%
+    select(-D) %>%
+    pivot_wider(names_from = "topic", values_from = "beta") 
+  
+  discriminative_terms %>% 
+    mutate(rank = 1:nrow(discriminative_terms)) %>% 
+    select(term,rank)
+})  
+
+
+
+#Header for quotes
+output$quote_header <- renderText({
+  if(input$data == 'Reviews'){
+  return('Two Good Reviews and Two Bad Reviews: <br><br>')}
+else{return('Two Important Tips: <br><br>')}
+})
+
+#Conditional quote generator from LDA analysis
+output$review_quotes <- renderText({
+  id <- business_ID()
+  if(input$data == 'Reviews'){
+    text_tibble <- quote_reviews %>% 
+      filter(business_id == id) %>%  
+      select(text)
+  }
+  else{
+    text_tibble <- tip_reviews %>% 
+      filter(business_id == id) %>%  
+      select(text)
+  }
+  
+  quotes <- ''
+  for(i in text_tibble[,1]){
+    temp <- paste0(i, '<br><br>')
+    quotes <- paste0(quotes,temp)
+  }
+quotes
+})
+
+
+##############End topics page logic
  vecfile<- reactive({
    paste0("word2vec_", input$choice, ".csv")
  })
